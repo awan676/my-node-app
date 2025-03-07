5b3e0f6d1ff2cc21ed75b22e7ec27d65088f70be
awan676 <aawanzada123@gmail.com>
2025-03-07 16:41:37 +0500

    Fix start script and port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
     res.send("Hello, World! 🚀 This is my Express app.");
