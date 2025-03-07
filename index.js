9878aef882b6a9acac1340e3af90cc7d57737fc7
awan676 <aawanzada123@gmail.com>
2025-03-07 17:06:59 +0500

    Fix port binding for Railway

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
     res.send("Hello, World! 🚀 This is my Express app.");
